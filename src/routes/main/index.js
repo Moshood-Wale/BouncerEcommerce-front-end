606d7a587b129a3bdf0e2aaa263b9b93709173dc
Moshood-wale <moshood815@gmail.com>
2021-10-27 19:58:26 +0100

    Created register-page and consumed register end-point

--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -4,7 +4,8 @@ import { Route, Switch } from "react-router-dom";
 import MyLoader from '../../utils/loader'
 
 import HomePage from '../../pages/HomePage'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // RegisterPage
 import RegisterPage from '../../pages/RegisterPage';
 
@@ -14,6 +15,7 @@ const App = () => (
       <MyLoader /> 
     </div>
   }>
+    <ToastContainer/>
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/register' component={RegisterPage} />
