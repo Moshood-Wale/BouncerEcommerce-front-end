e79fdd9b9da49fd3ddecc4ca97fe7afe0552d037
Moshood-wale <moshood815@gmail.com>
2021-11-02 12:26:21 +0100

    removed navbar link-homepage

--- src/components/register/NavBar.js
+++ src/components/register/NavBar.js
@@ -1,10 +1,7 @@
-import { Link } from 'react-router-dom';
-
-
 function NavBar()  {
     return (
         <div className="Logo" data-testid="logo">
-            <Link to="/home"><img src="./logo_alcxme.svg" alt="" /></Link>
+            <img src="./logo_alcxme.svg" alt="" />
         </div>
     )
 }
