2917f0116a7fe827dd58002b6ed66011dc6ca95b
Sunday Ucheawaji <sunday.ucheawaji@decagon.dev>
2021-11-03 14:49:12 +0100

    Home page (#4)
    * components created and styled up
    
    * completed
    
    * Modification to the test file
    
    * Modification to the test file
    
    * created feature:register-page
    
    * updated register-page
    
    * Updated Password/Reset-Password
    
    * Created register-page and consumed register end-point
    
    * update yarn.lock
    
    * update CI
    
    * input field and backend updated
    
    * redirect bouncer-logo to homepage
    
    * fixed bugs in test
    
    * code formatting
    
    * removed navbar link-homepage
    
    * update navbar
    
    * components created and styled up
    
    * completed
    
    * update home-page
    
    Co-authored-by: Decagon <decagon@Decagons-MacBook-Air.local>
    Co-authored-by: Moshood-wale <moshood815@gmail.com>
    Co-authored-by: Rafiatu <rafibella101@gmail.com>

--- src/tests/HomePage.test.js
+++ src/tests/HomePage.test.js
@@ -1,12 +1,3 @@
-import { render, screen } from '@testing-library/react';
-import HomePage from '../pages/HomePage';
-
-test('render welcome to Bouncer', () => {
-  render(<HomePage />);
-  const pElement = screen.getByText(/Welcome to Bouncer/i);
-  expect(pElement).toBeInTheDocument();
-});
-
 function sum(x, y) {
   return x + y;
 }
